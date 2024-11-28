976500f0d5fbea179156cd19bfb6b372548aaae5
ashokbhandaricom <ashokbhandari625@gmail.com>
2024-11-28 10:20:31 +0530

    API working skeleton

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+
+    [ApiController]
+
+    [Route("api/[controller]")] // /api/users
+    public class UsersController(DataContext context) : ControllerBase 
+    {
+        [HttpGet]
+
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
+          return await context.Users.ToListAsync(); 
+        }
+    [HttpGet("{id}")]
+        public async Task< ActionResult<AppUser>> GetUser(int id ){
+
+            if( await context.Users.FindAsync(id) == null ) return NotFound(); 
+          return await  context.Users.FindAsync(id);
+        }
+    }
+
+}
