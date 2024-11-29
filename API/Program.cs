32eae5aa55d008db6a0c332040cc2523eb431b18
ashokbhandaricom <ashokbhandari625@gmail.com>
2024-11-29 09:01:40 +0530

    ff

--- API/Program.cs
+++ API/Program.cs
@@ -14,9 +14,12 @@ builder.Services.AddDbContext<DataContext>(opt=> {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")); 
 }); 
 
+builder.Services.AddCors() ; 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin()) ; 
 
 app.MapControllers();
 
